62173a0052589723c74b1d62699181eb0a5b859e
Ahmed Waleed Malik <ahmed.waleed@aurorasolutions.io>
2019-12-30 16:27:11 +0500

    platform-refactored add variables for keycloak default user & password

--- scripts/configure-dev.sh
+++ scripts/configure-dev.sh
@@ -13,8 +13,17 @@ replace_values() {
   find ../config -type f -name "*.*" -print0 | xargs -0 sed -i -e "s#${1}#${VALUE_TO_REPLACE}#g"
 }
 
+replace_values CLOUD_PROVIDER $CLOUD_PROVIDER && \
+replace_values DOMAIN $DOMAIN && \
+replace_values SSL_CERTIFICATE_CA_CRT $SSL_CERTIFICATE_CA_CRT && \
+replace_values SSL_CERTIFICATE_TLS_CRT $SSL_CERTIFICATE_TLS_CRT && \
+replace_values SSL_CERTIFICATE_TLS_KEY $SSL_CERTIFICATE_TLS_KEY && \
 replace_values STAKATER_PLATFORM_SSH_GIT_URL $STAKATER_PLATFORM_SSH_GIT_URL && \
-replace_values KEYCLOAK_CLIENT_ID $KEYCLOAK_CLIENT_ID
+replace_values STAKATER_PLATFORM_BRANCH $STAKATER_PLATFORM_BRANCH && \
+replace_values USER_MAIL $USER_MAIL && \
+replace_values USER_NAME $USER_NAME && \
+replace_values REPO_ACCESS_TOKEN $REPO_ACCESS_TOKEN && \
+replace_values DOMAIN $DOMAIN
 
 
 # TODO once values are replaced convert all configs to base64 encoded values
