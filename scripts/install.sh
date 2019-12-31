81c010dbf50bc549c71ffe65b5a91e221aeb47fd
Ahmed Waleed Malik <ahmedwaleedmalik@gmail.com>
2019-12-31 16:11:45 +0500

    platform-refactored refactored install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -37,5 +37,5 @@ kubectl -n flux wait --timeout=200s --for condition=ready pod -l release=flux
 
 # Wait for dashboard to be ready & then print dashboard access token
 kubectl -n control wait --timeout=2000s --for condition=ready pod -l release=stakater-control-dashboard
-echo -e "\n========= Kubernetes Dashboard Access Token ==========\n"
+echo -e "\n========= Kubernetes Dashboard Access Token =========="
 kubectl -n control describe secret $(kubectl -n control get secret | grep stakater-control-dashboard-kubernetes-dashboard-token | awk '{print $1}') | grep 'token:' | cut -d ':' -f2
