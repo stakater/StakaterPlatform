89d0847a8d2a9054f9ded6ab73892f4b6e6f4b26
irti <aliartiza75@yahoo.com>
2019-12-31 16:07:31 +0500

    [platform-refactored] updated tests

--- scripts/tests/tracing-stack.sh
+++ scripts/tests/tracing-stack.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # NOTE: These tests require shunit2 to be installed
 testJaegerURL() {
-  x=$(curl -IL https://jaeger-tracing.DOMAIN 2>/dev/null | head -n 1 | cut -d ' ' -f2)
+  x=$(curl -X GET -IL https://jaeger-tracing.workshop.stakater.com 2>/dev/null | head -n 1 | cut -d ' ' -f2)
   assertEquals 200 $x
 }
 
