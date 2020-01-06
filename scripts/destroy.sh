62e91d41001a87fa7b54bc1109e25546bd2ea53b
usamaahmadkhan <usama.ahmad.khan@hotmail.com>
2020-01-06 16:32:14 +0500

    fix destroy script

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 NAMESPACES="flux control delivery logging monitoring security tracing"
 
+kubectl delete --wait=true -R -f platform/flux/
+
+# Delete all helm Releases and corresponding resources
+# along with release history in kube-system
+helm delete --purge $(helm list --short)
+
+# Remove any remaning platform resources
+kubectl delete --wait=true --ignore-not-found -R -f platform/
+
+# Remove remaining PVCs in all namespaces
+for NAMESPACE in $NAMESPACES; do
+  kubectl delete pvc --all -n $NAMESPACE
+done
+
 # Remove Helm Operator
 helm delete --purge helm-operator
 
 # Remove Tiller
 helm reset --force
 
-# Delete all HRs
-for NAMESPACE in $NAMESPACES; do
-  kubectl delete hr --all -n $NAMESPACE
-done
-
-# Remove any remaing platform resources
-kubectl delete -f platform/
-
-#TODO remove role and cluster roles
+# Manually delete helmRelease CRD. Temp fix. Will delete all crds.
+kubectl delete crd --all
 
 # Delete all namespaces
 kubectl delete namespaces $NAMESPACES
